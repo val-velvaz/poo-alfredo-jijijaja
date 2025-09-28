53a4f2557f983c8a6dcc609c5431f48493fe0c05
Valeria Velázquez <valeria.velazquez8571@alumnos.udg.mx>
2025-09-27 19:56:38 -0600

    agregando proyecto

---
+++ include/Name.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <ostream>
+
+class Name {
+private:
+    std::string first_name;
+    std::string last_name;
+public:
+    Name()  
+        : first_name(""), last_name("") {}
+    Name(const std::string& first_name, const std::string& last_name) 
+        : first_name(first_name), last_name(last_name) {}
+
+    const std::string& getFirstName() const;
+    const std::string& getLastName() const;
+
+    void setFirstName(const std::string& fn);
+    void setLastName(const std::string& ln);
+
+    Name& operator = (const Name& other);
+
+    friend std::ostream& operator << (std::ostream& os, const Name& other);
+    friend std::istream& operator >> (std::istream& is, Name& other);
+};
---
+++ include/Paciente.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Name.hpp"
+
+#include <string>
+
+class Paciente {
+    std::string social_security_number;
+    Name name;
+};
